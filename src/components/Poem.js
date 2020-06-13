19fdc86e6732a8534d18b6994a3dd8b56d04ad4b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-06-13 15:40:31 +0900

    [update] いらなそうなコード削除

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { text } from '@storybook/addon-knobs';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -43,7 +36,6 @@ const useStyles = makeStyles((theme) => ({
 
 export const Poem = ({accountId, text, image, postDate, likeCount}) =>  {
     const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
   
     return (
         <>
@@ -71,7 +63,7 @@ export const Poem = ({accountId, text, image, postDate, likeCount}) =>  {
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
-        {likeCount}
+        {}
       </CardActions>
     </Card>
         </>
