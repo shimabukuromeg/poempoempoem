50c421e950d128d705a7a23fef0a77771ad06661
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-06-13 15:37:43 +0900

    [update] 投稿のコンポーネントを作成した

---
+++ storybook-static/precache-manifest.92d05862444a8e96589d81fe448737d0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915ca0215d0e2b00fe077361c4fa03db",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c8afbbdcfb21eb97edfc.bundle.js"
+  },
+  {
+    "url": "runtime~main.c8afbbdcfb21eb97edfc.bundle.js"
+  },
+  {
+    "url": "vendors~main.c8afbbdcfb21eb97edfc.bundle.js"
+  },
+  {
+    "url": "vendors~main.c8afbbdcfb21eb97edfc.bundle.js.LICENSE.txt"
+  }
+]);
